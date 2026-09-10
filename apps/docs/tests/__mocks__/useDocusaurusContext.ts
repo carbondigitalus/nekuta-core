3c5dd1ac6c6fb33dde8bc70aaef6853a1fb5f1c1
Jared Ledbetter <jared@carbondigital.us>
2026-09-09 21:25:17 -0400

    Create useDocusaurusContext.ts

---
+++ apps/docs/tests/__mocks__/useDocusaurusContext.ts
@@ -0,0 +1,8 @@
+export default function useDocusaurusContext() {
+    return {
+        siteConfig: {
+            title: 'Veteran Database',
+            tagline: 'The official docs for our VeteranDB platform.'
+        }
+    };
+}
