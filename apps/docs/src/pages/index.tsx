e3bf52f5d1381d8e6456854c888330ccd452774d
Jared Ledbetter <jared@carbondigital.us>
2026-09-09 21:24:51 -0400

    Create index.tsx

---
+++ apps/docs/src/pages/index.tsx
@@ -0,0 +1,39 @@
+// NPM Modules
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
+
+// Custom Modules
+import './index.module.scss';
+
+function HomepageHeader() {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero')}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                {/* <div className={styles.buttons}>
+          <Link
+            className='button button--secondary button--lg'
+            to='/docs/intro'
+          >
+            Get Started
+          </Link>
+        </div> */}
+                {/* <h2>Content Coming Soon!</h2> */}
+            </div>
+        </header>
+    );
+}
+
+export default function Home(): React.JSX.Element {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout title={`Docs Home`} description={siteConfig.tagline}>
+            <HomepageHeader />
+            <main></main>
+        </Layout>
+    );
+}
