ae4a50ad2c39c7b438159fb9922c24b7398b7019
Jared Ledbetter <jared@carbondigital.us>
2026-09-10 14:15:09 -0400

    delete pinia

--- pinia/packages/nuxt/playground/layers/layer-domain/stores/basic.ts
+++
@@ -1,5 +0,0 @@
-export const useBasicStore = defineStore('layer-basic', () => {
-  const count = ref(0)
-
-  return { count }
-})
--- pinia/packages/nuxt/playground/stores/nested/some-store.ts
+++
@@ -1,4 +0,0 @@
-export const useSomeStoreStore = defineStore('some-store', () => {
-  // console.log('I was defined within a nested store directory')
-  return {}
-})
